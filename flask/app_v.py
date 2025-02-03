92effa6af4cfb0db10ee4718bddf6cd3ca8abff9
lontralontra <ianribeiroleite@usp.br>
2025-02-03 14:52:26 +0100

    lsdh

--- flask/app_v.py
+++ flask/app_v.py
@@ -193,7 +193,7 @@ app = Flask(__name__)
 def handle_websocket_message(message):
     print("json:" + str(message))
     value = message["Signal"][0]["Value"][0]
-    motor_value = message["Signal"][0]["Value"][0]
+    motor_value = message["Signal"][1]["Value"][0]
     print("motor value:"+ str(motor_value))
     print("value:"+ str(value))
     global last_10_received_values_sensor
