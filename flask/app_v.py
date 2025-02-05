a0393eb612fda9c32caafec56d6cc49c123e3ff7
lontralontra <ianribeiroleite@usp.br>
2025-02-05 15:48:56 +0100

    adfa

--- flask/app_v.py
+++ flask/app_v.py
@@ -238,12 +238,12 @@ def camera_view():
 #%def index_ancien():
 #    return render_template('test.html')
 
-#@app.route('/')
-#def index():
-#    return render_template('test3.html')
+@app.route('/new')
+def index_new():
+    return render_template('test3.html')
 
 @app.route('/')
-def index_ancien():
+def index():
     return render_template('test.html')
 
 
