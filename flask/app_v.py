baabce99c689013c92132c02b44bb9759257880e
solal <solal.raymondjean@gmail.com>
2025-02-05 14:26:07 +0100

    corr

--- flask/app_v.py
+++ flask/app_v.py
@@ -253,11 +253,11 @@ def l_camera():
 
 @app.route('/values_speed_camera', methods=['GET'])
 def l_speed_camera():
-    return jsonify(np.gradient(values[0], values[1]))
+    return jsonify([np.gradient(values[0], values[1]), values[1]])
 
 @app.route('/values_speed_sensor', methods=['GET'])
 def l_speed_sensor():
-    return jsonify(np.gradient(values_sensor[0], values_sensor[1]))
+    return jsonify([np.gradient(values_sensor[0], values_sensor[1]), values_sensor[1]])
 
 
 @app.route('/values_motor', methods=['GET'])
