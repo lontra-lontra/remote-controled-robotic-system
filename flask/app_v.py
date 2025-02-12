18a8fe2e4f31b6940e13e47eb8f17ff39fef58f4
solal <solal.raymondjean@gmail.com>
2025-02-12 15:52:49 +0100

    cam

--- flask/app_v.py
+++ flask/app_v.py
@@ -48,6 +48,7 @@ if config["camera"]:
     picam2.configure(
         picam2.create_preview_configuration(
         main={"size": (640, 480)},
+        raw={"size": picam2.sensor_resolution},  # Utilise toute la résolution du capteur
         buffer_count=2
     )
     )
